c949bf709903bab764d1bab647b1faac3cda42af
Garnet Bugbee <garnet.bugbee@gmail.com>
2021-03-19 14:44:33 -0700

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'b34658c277c9d86e1b3a11e100e84aca';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 45.512230;
+  let longitude = -122.658722;
 
   getWeather(latitude, longitude);
 }
